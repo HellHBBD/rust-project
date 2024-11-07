4056fb52c8e2f711f8fb7e9e9df5bab62bde09a3
Hell HBBD <paulyu602@gmail.com>
2024-11-07 15:21:48 +0800

    implement serialize and deserialize code

--- list/src/list.rs
+++ list/src/list.rs
@@ -1,19 +1,14 @@
-#[derive(Debug)]
+use serde::{Deserialize, Serialize};
+use std::fs;
+use std::io::Error;
+
+#[derive(Debug, Serialize, Deserialize)]
 struct Node {
     name: String,
     next: Option<Box<Node>>,
 }
 
-impl Node {
-    //pub fn new(value: &str) -> Self {
-    //    Self {
-    //        name: String::from(value),
-    //        next: None,
-    //    }
-    //}
-}
-
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct List {
     head: Option<Box<Node>>,
 }
@@ -45,5 +40,16 @@ impl List {
             current = next;
         }
         println!("{}", current.name);
+    }
+
+    pub fn write(self) {
+        let json_string = serde_json::to_string_pretty(&self).unwrap();
+        fs::write("data.json", json_string).unwrap();
+    }
+
+    pub fn read() -> Result<List, Error> {
+        let json_string = fs::read_to_string("data.json")?;
+        let list = serde_json::from_str(&json_string)?;
+        Ok(list)
     }
 }
--- list/src/main.rs
+++ list/src/main.rs
@@ -3,9 +3,13 @@ mod list;
 use crate::list::List;
 
 fn main() {
-    let mut list = List::new();
+    let mut list = match List::read() {
+        Ok(result) => result,
+        Err(_) => List::new(),
+    };
     list.add("hello");
     list.add("testing");
     list.add("world");
     list.print();
+    list.write();
 }
